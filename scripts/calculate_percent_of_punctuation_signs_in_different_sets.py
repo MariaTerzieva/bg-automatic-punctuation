30239bc04c2ebd55b94233e28f8ab1addb47e8ea
MariaTerzieva <mariya.g.terzieva@gmail.com>
2021-09-07 00:14:57 +0300

    Add a script that calculates the percent of punctuation signs in different sets

---
+++ scripts/calculate_percent_of_punctuation_signs_in_different_sets.py
@@ -0,0 +1,23 @@
+import sys
+import re
+from string import punctuation
+from collections import Counter
+
+
+punctuation_enhanced = punctuation + '\n–„“…'
+
+input_file_name = sys.argv[1]
+train_file_name = re.sub('\.txt', '_train.txt', input_file_name)
+dev_file_name = re.sub('\.txt', '_dev.txt', input_file_name)
+test_file_name = re.sub('\.txt', '_test.txt', input_file_name)
+
+counts = Counter(open(input_file_name).read())    
+input_file_punctuation_counts = {k:v for k, v in counts.items() if k in punctuation_enhanced}
+print(input_file_name, ':', input_file_punctuation_counts)
+
+for file_name in (train_file_name, dev_file_name, test_file_name):
+    counts = Counter(open(file_name).read())    
+    percent_of_input_file_punctuation_counts = {k:round(v/input_file_punctuation_counts[k]*100, 2) for k, v in counts.items() if k in punctuation_enhanced}
+    print(file_name, ':', percent_of_input_file_punctuation_counts)
+
+
